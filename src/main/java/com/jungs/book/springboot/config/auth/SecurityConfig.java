1629b56e83adbd041df651cf903710510417bc41
kimjunghak <kboy1025@gmail.com>
2020-02-04 10:36:40 +0900

    delete @override

--- src/main/java/com/jungs/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jungs/book/springboot/config/auth/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
